e4d5bb21b9253956dc56ae8ac02a6322dbd98982
Kaznarah1 <127631298+Kaznarah1@users.noreply.github.com>
2023-11-15 00:05:45 +0300

    need mailSender

---
+++ Agence-de-voyage/src/main/java/fytech/group/Agence/de/voyage/service/EmailService.java
@@ -0,0 +1,28 @@
+package fytech.group.Agence.de.voyage.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendConfirmationEmail(String to, String Object, String Content) throws MessagingException{
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+
+        helper.setTo(to);
+        helper.setSubject(Object);
+        helper.setText(Content,true);
+
+        javaMailSender.send(message);
+
+    }
+
+}
